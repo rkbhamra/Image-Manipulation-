d3aa68f1f9515f452d35ffe82891cd3bdbeef5b6
reetkaurbhamra <reet.rkb@gmail.com>
2024-10-08 19:36:51 -0400

    the hist_equalization function works now

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Functions in the tool:
 1. Negative of an image
 2. Average of an image
+![alt text](image.png)
--- hist_equ.py
+++ hist_equ.py
@@ -5,31 +5,72 @@ import matplotlib.pyplot as plt
 class HistogramEqualization:
     def __init__(self, image_path):
         self.image_path = image_path
+        self.image = Image.open(image_path)  
         if self.image is None:
             raise ValueError("Image not found or unable to load.")
         
-    def average_image(image_array):
-        if len(image_array.shape) == 3:
-            height, width, channels = image_array.shape
+    def average_image(self, image_array):
+        """Convert an RGB image to grayscale by averaging the RGB channels."""
+        if len(image_array.shape) == 3: 
             avg_img_array = []
             for i in image_array:
                 avg_img_array.append([])
                 for j in i:
-                    l = 0
+                    avg_pixel_value = np.uint16(0)
                     for k in j:
-                        l += k
-                    avg_img_array[-1].append(int(l/3))
-            return avg_img_array
+                        avg_pixel_value += k
+                    avg_img_array[-1].append(np.uint8(avg_pixel_value / 3)) 
+            return np.array(avg_img_array)
         else:
-            return image_array
-        return image_array
-
-    def equalize_histogram(image_array):
-        return 1
+            return image_array 
     
-    img = Image.open('img1.png')
-    img_array = np.array(img)
-    #print(average_image(img_array))
-    plt.imshow(average_image(img_array), cmap='gray') 
-    plt.show()
+    def count_pixels(self, image_array):
+        """Count occurrences of each pixel value (0-255) in the image."""
+        count_list = [0] * 256
+        height, width = image_array.shape
+        for i in range(height):
+            for j in range(width):
+                pixel_value = image_array[i][j]
+                count_list[pixel_value] += 1
+        return count_list
+    
+    def calc_probability(self, pixel_count_array):
+        """Calculate the probability of each pixel intensity."""
+        total_pixels = sum(pixel_count_array)
+        prob_list = [count / total_pixels for count in pixel_count_array]
+        return prob_list
 
+    def equalize_histogram(self):
+        """Equalize the pixel intensities of the image."""
+        img_array = np.array(self.image)
+        avg_img_array = self.average_image(img_array)  
+        pixel_counts = self.count_pixels(avg_img_array)  
+        probabilities = self.calc_probability(pixel_counts)  
+        s = []
+        s.append(round((len(probabilities)-1) * probabilities[0]))
+        for i in range(1, len(probabilities)):
+            s.append(round(((len(probabilities)-1) * probabilities[i]) + s[i-1]))
+
+        for i in range(avg_img_array.shape[0]):
+            for j in range(avg_img_array.shape[1]):
+                avg_img_array[i][j] = np.clip(s[int(avg_img_array[i][j])], 0, 255)
+        return avg_img_array
+
+    def apply_equalization(self):
+        """Equalize and display histogram of pixel intensities."""
+        img_array = np.array(self.image)
+        avg_img_array = self.average_image(img_array)  
+        equalized_image = self.equalize_histogram()  
+
+        #plot the original and equalized images
+        fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+        ax[0].imshow(avg_img_array, cmap='gray')
+        ax[0].set_title("Original Image")
+        ax[1].imshow(equalized_image, cmap='gray')
+        ax[1].set_title("Equalized Image")
+        plt.show()
+
+# Example usage
+image_path = 'img1.png'  
+hist_eq = HistogramEqualization(image_path) 
+hist_eq.apply_equalization()  
