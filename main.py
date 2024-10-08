e909b8b37551ed40d88b5d3b0b851f7eec1d4832
reetkaurbhamra <reet.rkb@gmail.com>
2024-10-07 21:19:07 -0400

    negative image function

--- main.py
+++ main.py
@@ -3,13 +3,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 img = Image.open('img1.png')
-# Convert image to numpy array
 img_array = np.array(img)
+plt.imshow(img_array, cmap='gray')  
 
-# Print the numpy array
-print(img_array)
+print(f"Image array shape: {img_array.shape}")
 
-# Display the image as a pixel value image
-plt.imshow(img_array, cmap='gray')
-plt.colorbar()
+'''def average_pixels(img_array):
+    if len(img_array.shape) == 3:
+        height, width, channels = img_array.shape
+        avg_img_array = img_array.mean(axis=2)  
+        return avg_img_array
+    else:
+        return img_array
+'''
+    
+def average_pixels(img_array2):
+    img_array3 = img_array2
+    for i in range(len(img_array3)):
+        for j in range(len(img_array3[i])):
+            for k in range(len(img_array3[i][j])):
+                img_array[i][j][k] = 255-img_array3[i][j][k]
+    return img_array3
+
+averaged_image = average_pixels(img_array)
+
+
+
+pixel_plot = plt.figure()
+
+plt.title("Pixel Plot")
+pixel_plot = plt.imshow(averaged_image, cmap='twilight', interpolation='nearest')
+
+plt.colorbar(pixel_plot)
+
+plt.savefig('pixel_plot.png')
+
 plt.show()
